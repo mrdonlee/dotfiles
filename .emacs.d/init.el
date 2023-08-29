2f13fc3d4258039510eff8a0a2f8b85299d46ac0
Don Lee <git@mrdonlee.addy.io>
2023-08-29 17:22:53 +0100

    style: makes visual changes for minimalism and code editor look.
    - Enables rainbow delimiter for brace identification
    - Disable menubar, scrollbar and toolbar
    - Enable line numbers
    - Applys gruvbox theme for editor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,10 +17,28 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil)))
+ '(custom-safe-themes
+   '("b1a691bb67bd8bd85b76998caf2386c9a7b2ac98a116534071364ed6489b695d" "2ff9ac386eac4dffd77a33e93b0c8236bb376c5a5df62e36d4bfa821d56e4e20" "d80952c58cf1b06d936b1392c38230b74ae1a2a6729594770762dc0779ac66b7" default))
+ '(package-selected-packages '(rainbow-delimiters gruvbox-theme evil)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Enable rainbow delimiter
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+(scroll-bar-mode -1) ; Disable scrollbar
+(tool-bar-mode -1) ; Disable toolbar
+(menu-bar-mode -1) ; Disable menu bar
+
+(load-theme 'gruvbox-dark-hard)
+
+;; Enable line number.
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+;; Disable line number for some modes
+
