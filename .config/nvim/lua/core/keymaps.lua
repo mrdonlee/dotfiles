0e35dedc578742c85ee47b6d6b00d6f88d8f4c50
Don Lee <git@mrdonlee.addy.io>
2024-08-06 20:00:04 +0100

    fix: fix breaking changes

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -16,3 +16,9 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 
 -- [[ Open Lazygit ]]
 vim.keymap.set('n', '<leader>lg', ":FloatermNew --height=47 --width=166 lazygit<CR>", { desc = 'Open lazygit' })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
