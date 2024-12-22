eee53f9e76cff0e420a1a8c02e9501a11f35a2eb
Don Lee <git@mrdonlee.addy.io>
2024-12-22 13:48:18 +0000

    feat(tabstop): sets tabstop adn shiftwidth for go files

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -64,11 +64,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 vim.o.smartindent = true
 vim.o.autoindent = true
 
--- vim.api.nvim_create_autocmd("FileType", {
-  --   pattern = "go",
-    -- callback = function()
-      --   vim.opt_local.tabstop = 4
-        -- vim.opt_local.shiftwidth = 4
-        -- vim.opt_local.expandtab = false
-    -- end,
--- })
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "go",
+    callback = function()
+        vim.opt_local.tabstop = 4
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.expandtab = false
+    end,
+})
