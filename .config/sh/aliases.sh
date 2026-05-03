1a5295a102f4940b20dca6b4729c32bfb04d9555
Don Lee <git@mrdonlee.addy.io>
2026-05-03 19:00:20 +0100

    feat(alias): add fzf aliases

--- .config/sh/aliases.sh
+++ .config/sh/aliases.sh
@@ -2,3 +2,7 @@ alias ls='ls --color=auto'
 alias ll='ls -alF'
 alias la='ls -A'
 alias l='ls -CF'
+
+# fzf aliases
+alias f='fzf --preview "bat --style=numbers --color=always {}"'
+alias edit='fzf --preview "bat --style=numbers --color=always {}" | xargs -r $EDITOR'
